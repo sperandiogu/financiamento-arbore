4291c524c772ee4e3129ddd3b3debe714430b3af
Gustavo Sperandio <gusperandio2@gmail.com>
2024-07-05 15:30:16 -0300

    ajuste de estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Cadastro from './components/cadastro.tsx';
+import Dashboard from './components/Dashboard.tsx';
 import Header from './components/Header.tsx';
 import Simulador from './components/simulador.tsx';
 import StepProgress from './components/StepProgress.tsx';
@@ -23,6 +24,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Cadastro onNext={handleNext} onBack={handleBack} currentStep={currentStep} />} />
         <Route path="/simulador" element={<Simulador onNext={handleNext} onBack={handleBack} currentStep={currentStep} />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
